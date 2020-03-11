7c8ceb419fe48d68930a3ab38eff5a78a5ed0858
GeorgeSearz <outerheaven37@protonmail.com>
2020-03-10 20:14:08 -0700

    push

---
+++ client/build/precache-manifest.68edddf5200efbcb006dda14ab100af8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63a25525f04af8a3f5846ea5b9f74f5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f5bf0d5091256cfa314",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "99c3a51d2ab1eacd0868",
+    "url": "/static/js/2.43e0b3ed.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.43e0b3ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f5bf0d5091256cfa314",
+    "url": "/static/js/main.ea6d566e.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  }
+]);
